54b5b7033ec05619a5415e87b699f035cc433627
yurickvinicius <yurickvinicius@gmail.com>
2018-09-12 02:47:48 -0300

    update title home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - Welcome to UTFPR!
+                    Laravel - Welcome to UTFPR
                 </div>
 
                 <div class="links">
